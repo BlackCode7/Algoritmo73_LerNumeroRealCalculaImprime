4287a6b242a91617573ae870ab01f6c0e6a241a0
Anderson <BlackCode7@outlook.com>
2022-06-18 14:35:59 -0300

    First commit reading number real algorithm

---
+++ src/Algoritmo73_LerNumeroRealCalculaImprime/Algoritmo73_LerNumeroRealCalculaImprime.java
@@ -0,0 +1,10 @@
+package Algoritmo73_LerNumeroRealCalculaImprime;
+
+public class Algoritmo73_LerNumeroRealCalculaImprime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
