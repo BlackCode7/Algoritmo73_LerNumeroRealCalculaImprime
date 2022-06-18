9756f1fd61994fb0d806d92fb3e91763f408bab4
Anderson <BlackCode7@outlook.com>
2022-06-18 16:18:23 -0300

    Criar um algoritmo que receba um número real, calcular e imprimir: • a parte inteira do número • a parte fracionária do número • o número arredondado

--- src/Algoritmo73_LerNumeroRealCalculaImprime/Algoritmo73_LerNumeroRealCalculaImprime.java
+++ src/Algoritmo73_LerNumeroRealCalculaImprime/Algoritmo73_LerNumeroRealCalculaImprime.java
@@ -1,10 +1,29 @@
 package Algoritmo73_LerNumeroRealCalculaImprime;
 
+import java.util.Scanner;
+
 public class Algoritmo73_LerNumeroRealCalculaImprime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		algoritmo73_LerNumeroRealCalculaImprime();
+	}	
+		
+	public static void algoritmo73_LerNumeroRealCalculaImprime(){
+		
+		/* Criar um algoritmo que receba um número real, calcular e imprimir:
+		• a parte inteira do número • a parte fracionária do número • o número arredondado */
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Entre com um numero com parte fracionaria: ");
+		
+		double numReal = scanner.nextDouble();
+		double partInt = (long)numReal;		
+		double parte = numReal - partInt;
+		
+		System.out.println("Parte real digitado: " + numReal);
+		System.out.println("Parte parte inteira: "+ partInt);
+		System.out.println("Parte parte:" + parte);
+		
 	}
 
 }
